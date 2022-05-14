2e501972db415b53d8ece7bc43a9bb757d5f421b
unknown <KPajunurm@gmail.com>
2022-05-14 11:02:58 +0300

    Wishlist

--- model/wish.js
+++ model/wish.js
@@ -4,8 +4,9 @@ const filePath = path.join(path.dirname(require.main.filename), 'data', 'wishes.
 
 module.exports = class Wish {
 
-    constructor(wish) {
-        this.description = wish
+    constructor(wish, imageUrl) {
+        this.description = wish;
+        this.imageUrl = imageUrl;
     }
 
     saveWish() {
